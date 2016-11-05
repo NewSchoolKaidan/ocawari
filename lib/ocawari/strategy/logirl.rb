d7bf3b7ced1dddba772a18a82ecf8c30c7f37de9
Kenneth Uy <missingno15@gmail.com>
2016-11-05 16:02:31 -0400

    Move convenience methods to #json_response
    * #article_id and #api_endpoint weren't being used anywhere else so put it in a begin..end block with #json_response

--- lib/ocawari/strategy/logirl.rb
+++ lib/ocawari/strategy/logirl.rb
@@ -22,16 +22,12 @@ module Ocawari
         end
       end
 
-      def article_id
-        uri.to_s[/\d+/]
-      end
-
-      def api_endpoint 
-        URI("https://ucon.favclip.com/api/article/#{article_id}?corePortalID=logirl&visibility=site")
-      end
-
       def json_response
-        @json_response ||= JSON.parse(Net::HTTP.get(api_endpoint))
+        @json_response ||= begin
+          article_id = uri.to_s[/\d+/]
+          api_endpoint = URI("https://ucon.favclip.com/api/article/#{article_id}?corePortalID=logirl&visibility=site")
+          JSON.parse(Net::HTTP.get(api_endpoint))
+        end
       end
 
       def append_https(url_fragment)
