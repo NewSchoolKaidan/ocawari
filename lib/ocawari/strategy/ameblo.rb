7a8c952ee587a94fcc0759cb139c445c5de110a4
Kenneth Uy <missingno15@gmail.com>
2018-04-25 23:25:02 -0400

    Update Ameblo
    * Ameblo has started using React for their frontend which means scraping for the initial json state data instead of the img nodes

--- lib/ocawari/strategy/ameblo.rb
+++ lib/ocawari/strategy/ameblo.rb
@@ -10,7 +10,16 @@ module Ocawari
       ]
 
       def parse
-        image_nodes = page.css(CSS_HIERARCHY_SELECTORS.join(", "))
+        script_tag = page.css("script").find { |script| script.text.include?("window.INIT_DATA") }
+
+        image_nodes = script_tag.text.
+          split(";window")[0].
+          sub("window.INIT_DATA=", "").
+          yield_self { |raw| JSON.parse(raw) }.
+          dig("entryState", "entryMap", entry_id, "entry_text").
+          yield_self { |html_fragment| Nokogiri::HTML(html_fragment) }.
+          yield_self { |document| document.css("img") }.
+          select { |img| img["src"].include?("/user_images/") }
 
         image_nodes.map do |img|
           image_source = img["src"]
@@ -23,6 +32,11 @@ module Ocawari
             image_source
           end
         end
+      end
+
+
+      def entry_id
+        uri.to_s.split("/").last.sub("entry-", "").sub(".html", "")
       end
     end
   end
