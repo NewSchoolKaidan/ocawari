ff14731e623a3da2858edfff696e60c2ab7afe5b
Kenneth Uy <missingno15@gmail.com>
2018-07-03 11:20:07 -0400

    Fix Dwango Parser - 100 commits🎉

--- lib/ocawari/strategy/news_dwango.rb
+++ lib/ocawari/strategy/news_dwango.rb
@@ -4,20 +4,13 @@ module Ocawari
 
       private
 
+      CSS_SELECTORS = [
+        "#js-fancy-slider-sub img"
+      ]
+🎉
       def parse
-        script = page.css("script").find do |script|
-          /Config\.post/.match?(script.text)
-        end        
-
-        scrubbed_json = script.text.
-                          gsub(/[[:space:]]/, "").
-                          sub("window.jpnewsConfig.post=", "").
-                          sub(/;window\.jpnewsConfig\.prPosts.*$/, "") 
-
-        metadata = JSON.parse(scrubbed_json)
-
-        [metadata.dig("main_image", "full", "url")] + metadata["child_images"].map do |child_image|
-          child_image.dig("full", "url")
+        page.css(CSS_SELECTORS.join(",")).map do |img|
+          img["src"].sub("/lg_", "/")
         end
       end
     end
--- test/strategy/news_dwango_test.rb
+++ test/strategy/news_dwango_test.rb
@@ -4,7 +4,7 @@ require "ocawari/strategy/natalie"
 class NewsDwangoTest < Minitest::Test
   def test_returns_an_array
     VCR.use_cassette "news_dwango/oonuki-sayaka-8" do
-      uri = Addressable::URI.parse("https://news.dwango.jp/2017/01/27/120502/gravure/")
+      uri = Addressable::URI.parse("https://news.dwango.jp/gravure/20204-1701")
       strategy = Ocawari::Strategy::NewsDwango.new(uri)
       results = strategy.execute
 
@@ -14,9 +14,7 @@ class NewsDwangoTest < Minitest::Test
 
   def test_returns_expected_amount_of_images
     VCR.use_cassette "news_dwango/oonuki-sayaka-8" do
-      uri = Addressable::URI.parse("https://news.dwango.jp/2017/01/27/120502/gravure/")
-      strategy = Ocawari::Strategy::NewsDwango.new(uri)
-      images = strategy.execute
+      images = Ocawari.parse("https://news.dwango.jp/gravure/20204-1701")
 
       assert_equal 8, images.count
     end
