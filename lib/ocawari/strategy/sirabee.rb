00b7095a01c53e83aaf04e49e90fd2b7e302a8e4
Kenneth Uy <missingno15@gmail.com>
2018-10-02 20:49:03 -0400

    Update Sirabee parser

--- lib/ocawari/strategy/sirabee.rb
+++ lib/ocawari/strategy/sirabee.rb
@@ -4,14 +4,25 @@ module Ocawari
 
       private
 
-      CSS_SELECTORS = [
-        "article#entry section.entryContent img.size-large"
-      ]
+      LARGEST_KNOWN_WIDTH = 768
+      LARGEST_KNOWN_HEIGHT = 512
 
       def parse
-        page.css(CSS_SELECTORS.join(", ")).map do |image|
-          "http://#{image["src"].sub(/^\/\//, "")}"
+        if has_gallery_images?
+          page.css("div.entryGallery div.entryGallery-item img").map do |img|
+            img["src"].sub(/-\d{3}x\d{3}/, "-#{LARGEST_KNOWN_WIDTH}x#{LARGEST_KNOWN_HEIGHT}")
+          end
+        else
+          page.css("article.entryContent section.entryContentBody img.size-large").map do |img|
+            img["src"]
+          end
         end
+      end
+
+      private
+
+      def has_gallery_images?
+        page.css("div.entryGallery div.entryGallery-item img").any?
       end
     end
   end
--- test/strategy/sirabee_test.rb
+++ test/strategy/sirabee_test.rb
@@ -4,19 +4,39 @@ require "ocawari/strategy/sirabee"
 class SirabeeTest < Minitest::Test
   def test_returns_array
     VCR.use_cassette "sirabee/ayakawa-hinano-8" do
-      uri = Addressable::URI.parse("http://sirabee.com/2017/03/05/20161076169/")
-      strategy = Ocawari::Strategy::Sirabee.new(uri)
-      results = strategy.execute
+      url = "http://sirabee.com/2017/03/05/20161076169/"
+      images = Ocawari.parse(url)
 
-      assert_equal Array, results.class
+      assert_equal Array, images.class
+    end
+  end
+
+  def test_http_scheme_is_valid
+    VCR.use_cassette "sirabee/ayakawa-hinano-8" do
+      url = "http://sirabee.com/2017/03/05/20161076169/"
+      images = Ocawari.parse(url)
+
+      http_scheme_is_not_duplicated = images.all? do |image|
+        image.scan("http").count == 1
+      end
+
+      assert http_scheme_is_not_duplicated
     end
   end
 
   def test_returns_expected_amount_of_image_urls
     VCR.use_cassette "sirabee/ayakawa-hinano-8" do
-      uri = Addressable::URI.parse("http://sirabee.com/2017/03/05/20161076169/")
-      strategy = Ocawari::Strategy::Sirabee.new(uri)
-      images = strategy.execute
+      url = "http://sirabee.com/2017/03/05/20161076169/"
+      images = Ocawari.parse(url)
+
+      assert_equal 8, images.count
+    end
+  end
+
+  def test_page_with_gallery
+    VCR.use_cassette "sirabee/ayakawa-hinano-8-20180930" do
+      url = "https://sirabee.com/2018/09/30/20161815968/"
+      images = Ocawari.parse(url)
 
       assert_equal 8, images.count
     end
